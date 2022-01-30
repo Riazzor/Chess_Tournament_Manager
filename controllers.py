dbfec5164c126fdbad8eb241e4889d7cf6faa591
Florent SANIKA <florent.sanika@budget-insight.com>
2022-01-30 01:47:20 +0100

    [develop] Match
    Creating Match model and connecting in
    the controller.

--- controllers.py
+++ controllers.py
@@ -1,5 +1,6 @@
 
-from models import Player, Tournament
+from typing import List
+from models import Match, Player, Tournament
 from views import View
 
 
@@ -7,14 +8,35 @@ class Controller:
     def __init__(self, view) -> None:
         self.view = view
 
-    def run(self):
-        tournament = self.create_tournament()
-        for _ in range(8):
+    def run(self) -> None:
+        self.create_tournament()
+        # Player
+        for _ in range(2):
             player = self.create_player()
-            tournament.add_player(player)
-        print(tournament)
+            self.tournament.add_player(player)
+        print(self.tournament)
 
-    def create_tournament(self):
+        # Match
+        self.sort_for_match()
+        match_list = self.create_matchs()
+        print(match_list)
+
+        return None
+
+    def sort_for_match(self) -> None:
+        """
+        At the end of each round, we sort the players
+        according to their score.
+        If two players have the same score,
+        we use their rank.
+        """
+        players_list = sorted(
+            self.tournament.players,
+            key=(lambda player: (player.score, player.rank))
+        )
+        self.tournament.players = players_list
+
+    def create_tournament(self) -> None:
         tournament_info = self.view.get_tournament_info()
         tournament = Tournament(
             tournament_info['name'],
@@ -22,26 +44,69 @@ class Controller:
             tournament_info['date'],
             tournament_info['description'],
         )
-        return tournament
+        self.tournament = tournament
 
-    def create_player(self):
+        return None
+
+    def create_player(self) -> Player:
         player_info = self.view.get_player_info()
         player = Player(
             player_info['name'],
             player_info['surname'],
             player_info['birthdate'],
             player_info['gender'],
-            player_info['rank'],
-            player_info['score']
+            int(player_info['rank']),
+            int(player_info['score'])
         )
+
         return player
+
+    def create_matchs(self) -> List[Match]:
+        """
+        The swiss system is used :
+        The list of player (after being sorted) is split in
+        two sublist (S1 and S2).
+        The first of S1 plays against the first of S2,
+        the second of S1 against the second of S2, and
+        so on so that the last player of S1
+        plays against the last player of S2.
+        """
+        players_list = self.tournament.players
+        middle = len(players_list) // 2
+        is_not_even = len(players_list) % 2
+        firs_half, second_half = players_list[:middle], players_list[middle:]
+        match_list = [
+            (player1, player2)
+            for player1, player2 in zip(firs_half, second_half)
+        ]
+        matchs = list()
+        for match in match_list:
+            player1, player2 = match
+            matchs.append(
+                Match(player1, player2)
+            )
+
+        # should not happen but this part handles an odd number of players :
+        if is_not_even:
+            from random import choice
+            # a random player will be choosed to play a second time.
+            matchs.append(
+                Match(
+                    players_list[-1],           # player1
+                    choice(players_list[:-1])   # player2
+                )
+            )
+            del choice
+
+        return matchs
 
 
 if __name__ == '__main__':
     view = View()
     controller = Controller(view)
+    controller.run()
     # tournament = controller.create_tournament()
     # print(tournament)
 
-    player = controller.create_player()
-    print(player.__dict__)
+    # player = controller.create_player()
+    # print(player.__dict__)
--- models.py
+++ models.py
@@ -10,6 +10,9 @@ class Tournament:
     def add_player(self, player):
         self.players.append(player)
 
+    def __repr__(self) -> str:
+        return f'Tournament : {self.name} {self.description}'
+
 
 class Player:
     def __init__(self, name, surname, birthdate, gender, rank, score=0) -> None:
@@ -19,3 +22,32 @@ class Player:
         self.gender = gender
         self.rank = rank
         self.score = score
+
+    def __repr__(self) -> str:
+        return f'Player : {self.name} {self.surname}'
+
+
+class Match:
+    """
+    A match contains two lists.
+    Each list contains two elements :
+        - one reference to the player
+        - the player's score at the end of the match.
+
+    The init takes just the player and the score is entered
+    by the referee at the end of the game.
+    """
+
+    def __init__(self, player1: Player, player2: Player) -> None:
+        self.player1 = [player1]
+        self.player2 = [player2]
+
+    def set_score(self, score1, score2):
+        self.player1.append(score1)
+        self.player2.append(score2)
+
+    def __repr__(self) -> str:
+        return f'Match : {self.player1[0].name}  -vs-  {self.player2[0].name}'
+
+    def __str__(self) -> str:
+        return f'Match : {self.player1[0].name}  -vs-  {self.player2[0].name}'
